0ce920aa6ade04629b957da7ca9e6070d81bf99e
Anastasia Kiseleva <a.a.kisseleff@yandex.ru>
2022-04-28 07:54:51 +0300

    второе ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,24 @@
-let popup = document.querySelector('.popup');
-let profileEditButton = document.querySelector('.profile__edit-button');
-let popupExitButton = popup.querySelector('.popup__close-button');
-let formElement = document.querySelector('.popup__form');
-let popupFieldName = popup.querySelector('.popup__field_name');
-let popupFieldDescription = popup.querySelector('.popup__field_description');
-let profileName = document.querySelector('.profile__title');
-let profileDescription = document.querySelector('.profile__description');
+const popup = document.querySelector('.popup');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popupExitButton = popup.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__form');
+const popupFieldName = popup.querySelector('.popup__field_name');
+const popupFieldDescription = popup.querySelector('.popup__field_description');
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
 
-let popupToggle = function() {
+const popupToggle = function () {
   popup.classList.toggle('popup_opened');
 }
 
-profileEditButton.addEventListener('click', popupToggle);
-popupExitButton.addEventListener('click', popupToggle);
-
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = popupFieldName.value;
   profileDescription.textContent = popupFieldDescription.value;
   popupToggle();
 }
+
+profileEditButton.addEventListener('click', popupToggle);
+popupExitButton.addEventListener('click', popupToggle);
 
 formElement.addEventListener('submit', formSubmitHandler);
